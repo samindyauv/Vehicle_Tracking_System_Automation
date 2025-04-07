d8dca21be115cb8a2eb5af5c283699a5d8deb495
Samindya Vass <samindyauv@gmail.com>
2025-04-07 17:51:52 +0530

    delete geofence

--- src/test/java/Positive/Geofence.java
+++ src/test/java/Positive/Geofence.java
@@ -19,7 +19,7 @@ public class Geofence extends baseTest {
     }
 
     @Test(priority = 1)
-    public void CreateGeofence() throws InterruptedException {
+    public void CreatePolygonGeofence() throws InterruptedException {
         extentReportManager.startTest("Geofence Configuration Operations", "<b> Create Geofence – Polygon Shape </b>");
         extentReportManager.testSteps("<b><font color='blue'>Test Case : </font>TC01: Verify that the user can successfully create a geofence using a polygon shape</b>");
         extentReportManager.testSteps("<b><font color='blue'>Test Steps : </font></b>Logged to System > Click Tool > Click Geofencing > Add Geofence");
@@ -41,10 +41,23 @@ public class Geofence extends baseTest {
         webSteps.scrollToElement("Geo_saveBtn");
         webSteps.click("Geo_saveBtn");
 
-        Assert.assertEquals("Geo fence created successfully", webSteps.getText("Toast_message"));
+        Assert.assertEquals("Geofence has been created successfully!", webSteps.getText("Toast_message"));
     }
 
     @Test(priority = 2)
+    public void deleteGeofence() throws InterruptedException {
+        extentReportManager.startTest("Geofence Configuration Operations", "<b> Delete Geofence </b>");
+        extentReportManager.testSteps("<b><font color='blue'>Test Case : </font>TC01: Verify the user can delete an existing geofence</b>");
+        extentReportManager.testSteps("<b><font color='blue'>Test Steps : </font></b>Logged to System > Click Tool > Click Geofencing > Click Action > Click Delete");
+
+        webSteps.click("Geo_sb_action_btn");
+        webSteps.click("Geo_sb_delete_btn");
+        webSteps.scrollToElement("confirmation_delete_btn");
+        webSteps.click("confirmation_delete_btn");
+        Assert.assertEquals("GeoFence deleted successfully", webSteps.getText("Toast_message"), "Passed");
+    }
+
+    @Test(priority = 3)
     public void CreateRoundedGeofence() throws InterruptedException {
         extentReportManager.startTest("Geofence Configuration Operations", "<b> Create Geofence – Circle Shape </b>");
         extentReportManager.testSteps("<b><font color='blue'>Test Case : </font>TC01: Verify that the user can create a geofence using a circular shape</b>");
